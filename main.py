cad3de5edc809fb25e4183ea38a5df51d332001e
Michael Mink <michael.mink@daimler.com>
2022-02-11 21:33:27 +0100

    add

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from torchvision import models
 from torchvision import transforms
 import torch
+from PIL import Image
+
 
 # list available models
 dir(models)
@@ -18,4 +20,31 @@ transform = transforms.Compose([            #[1]
  std=[0.229, 0.224, 0.225]                  #[7]
 )])
 
+# Import image
+#img = Image.open('hase.JPG')
+img = Image.open('baum_eiche.JPG')
 
+# Pre-Process Image
+img_t = transform(img)
+batch_t = torch.unsqueeze(img_t, 0)
+
+# Model Inference
+alexnet.eval()
+
+out = alexnet(batch_t)
+print(out.shape)
+
+# Read the labels/classes
+with open('alexnet_labels.txt') as f:
+  classes = [line.strip() for line in f.readlines()]
+
+
+_, index = torch.max(out, 1)
+
+percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
+
+print(classes[index[0]], percentage[index[0]].item())
+
+
+_, indices = torch.sort(out, descending=True)
+print([(classes[idx], percentage[idx].item()) for idx in indices[0][:5]])
