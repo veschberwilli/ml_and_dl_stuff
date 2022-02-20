bb2afe08ee1613452a03546371ce96062f8408c1
Michael Mink <michael.mink@daimler.com>
2022-02-20 21:15:46 +0100

    add

---
+++ tmp/tmp.py
@@ -0,0 +1,23 @@
+
+# from https://pytorch.org/hub/ultralytics_yolov5/
+
+import torch
+
+# Model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+# Images
+imgs = ['https://ultralytics.com/images/zidane.jpg']  # batch of images
+
+# Inference
+results = model(imgs)
+
+# Results
+#results.print()
+#results.save()  # or .show()
+
+#results.xyxy[0]  # img1 predictions (tensor)
+tmp = results.pandas().xyxy[0]  # img1 predictions (pandas)
+print(tmp)
+
+
